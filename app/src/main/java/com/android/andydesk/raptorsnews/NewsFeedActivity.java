38278e9000f8c94a54e6211b31ca51300d97a82f
andyzen619 <andyzen619@gmail.com>
2018-03-22 21:54:26 -0400

    Initialization

---
+++ app/src/main/java/com/android/andydesk/raptorsnews/NewsFeedActivity.java
@@ -0,0 +1,13 @@
+package com.android.andydesk.raptorsnews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsFeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_feed);
+    }
+}
